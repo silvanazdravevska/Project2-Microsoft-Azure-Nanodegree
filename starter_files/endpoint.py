07bdcb07b0dd7a7034f4303e76e9bcba4367e196
silvanazdravevska <63349918+silvanazdravevska@users.noreply.github.com>
2021-01-11 21:08:49 +0000

    Add files via upload

--- starter_files/endpoint.py
+++ starter_files/endpoint.py
@@ -3,14 +3,14 @@ import json
 
 # URL for the web service, should be similar to:
 # 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
-scoring_uri = 'http://c5362d10-9d67-4def-8fcf-b5aae4793bcc.southcentralus.azurecontainer.io/score'
+scoring_uri = "http://c5362d10-9d67-4def-8fcf-b5aae4793bcc.southcentralus.azurecontainer.io/score"
 # If the service is authenticated, set the key or token
-key = 'vGa7TStLe4rGchXjWlBxP5Awp5NgQmIP'
+key = "vGa7TStLe4rGchXjWlBxP5Awp5NgQmIP"
 
 # Two sets of data to score, so we get two results back
-data = {"data":
-        [
-          {
+data = {
+    "data": [
+        {
             "age": 17,
             "campaign": 1,
             "cons.conf.idx": -46.2,
@@ -53,9 +53,9 @@ data = {"data":
             "pdays": 999,
             "poutcome": "failure",
             "previous": 1
-          },
-      ]
-    }
+        },
+    ]
+}
 # Convert to JSON string
 input_data = json.dumps(data)
 with open("data.json", "w") as _f:
